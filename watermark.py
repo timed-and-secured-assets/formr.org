e320909f97d6cfa097df142db6931c21f7752bca
Tom Schneider <tomschneider52@me.com>
2024-01-30 15:53:53 +0100

    added comments and restructured code for the watermark extraction

--- watermark.py
+++ watermark.py
@@ -80,7 +80,7 @@ def embed_watermark(image, watermark):
     Set up the watermark and assert it can be embedded into the image. 
     """
     # Convert the grayscale watermark image into an array of bits (booleans), shuffle it and store the size
-    watermark_bits = watermark.flatten() > 128
+    watermark_bits = watermark.flatten() > 127
     np.random.RandomState(rng_seed).shuffle(watermark_bits)
     watermark_size = watermark_bits.size
 
@@ -174,77 +174,130 @@ def extract_watermark(image, watermark_shape):
     :param watermark_shape: The shape of the watermark.
     :return: The extracted watermark.
     """
-    # Initialization
-    password_wm = 1
 
-    block_shape = np.array([4, 4])
-    password_img = 1
+    """
+    Set the parameters for the watermark extraction.
+    Note that these parameters must be identical to the parameters used in the embedding of the watermark.
+    """
+    # Parameters for the embedding strength (larger parameters are more stable but the output is more distorted)
     d1 = 36
     d2 = 20
 
+    # Shape of the blocks used in the embedding (dimensions must be equal and even)
+    block_shape = np.array([4, 4])
+
+    # Seed to use for the RNG (used to shuffle watermark data and embedding indices)
+    rng_seed = 1
+
+    """
+    Read the image to extract the watermark from.
+    """
+    # Extract the alpha channel (transparency) from the image if it is present
+    if image.shape[2] == 4 and image[:, :, 3].min() < 255:
+        image = image[:, :, :3]
+
+    # Convert the image to float and YUV color space
+    image = image.astype(np.float32)
+    image_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+
+    # Add a padding to the image to make the dimensions even (needed for DWT)
+    image_yuv = cv2.copyMakeBorder(image_yuv, 0, image.shape[0] % 2, 0, image.shape[1] % 2,
+                                   cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+    """
+    Initialize data structures for the Discrete Wavelet Transform.
+    """
+    # Approximation coefficients array (channel-wise)
+    # These coefficients represent the low-frequency components of the image and store the embedded watermark
     ca = [np.array([])] * 3
-    hvd = [np.array([])] * 3
+
+    # Block-wise approximation coefficients array (channel-wise)
     ca_block = [np.array([])] * 3
 
-    wm_shape = watermark_shape
-
-    # Extract watermark
-    wm_size = np.array(wm_shape).prod()
-
-    if image.shape[2] == 4:
-        if image[:, :, 3].min() < 255:
-            image = image[:, :, :3]
-
-    image = image.astype(np.float32)
-    img_shape = image.shape[:2]
-
-    img_yuv = cv2.copyMakeBorder(cv2.cvtColor(image, cv2.COLOR_BGR2YUV), 0, image.shape[0] % 2, 0,
-                                 image.shape[1] % 2, cv2.BORDER_CONSTANT, value=(0, 0, 0))
-
-    ca_shape = [(i + 1) // 2 for i in img_shape]
-
+    # Calculate the shapes of the ca and block-wise ca array
+    ca_shape = [(i + 1) // 2 for i in image.shape[:2]]
     ca_block_shape = (ca_shape[0] // block_shape[0], ca_shape[1] // block_shape[1], block_shape[0], block_shape[1])
+
+    # Calculate strides that are needed to transform the ca array into the block-wise ca array
     strides = 4 * np.array([ca_shape[1] * block_shape[0], block_shape[1], ca_shape[1], 1])
 
+    # Horizontal, vertical, and diagonal coefficients array (channel-wise)
+    # These coefficients represent the high-frequency components of the image
+    hvd = [np.array([])] * 3
+
+    """
+    Perform channel-wise Discrete Wavelet Transform on the YUV image.
+    """
     for channel in range(3):
-        ca[channel], hvd[channel] = pywt.dwt2(img_yuv[:, :, channel], 'haar')
+        ca[channel], hvd[channel] = pywt.dwt2(image_yuv[:, :, channel], 'haar')
+        # Transform the ca array into the block-wise ca array
+        # TODO: Think of a safer way to do this (see notes to np.lib.stride_tricks.as_strided)
         ca_block[channel] = np.lib.stride_tricks.as_strided(ca[channel].astype(np.float32), ca_block_shape, strides)
 
-    block_num = ca_block_shape[0] * ca_block_shape[1]
-    assert wm_size < block_num, IndexError(
-        'Available {}kb，Watermark{}kb'.format(block_num / 1000, wm_size / 1000))
-    block_index = [(i, j) for i in range(ca_block_shape[0]) for j in range(ca_block_shape[1])]
+    """
+    Initialize data structures for the watermark and assert it can be extracted from the image. 
+    """
+    # Calculate the size of the watermark
+    watermark_size = np.array(watermark_shape).prod()
 
-    wm_block_bit = np.zeros(shape=(3, block_num))
+    # Calculate the available block size (number of blocks in the image)
+    block_size = ca_block_shape[0] * ca_block_shape[1]
 
-    idx_shuffle = np.random.RandomState(password_img).random(size=(block_num, block_shape[0] * block_shape[1])) \
+    # Array to store the extracted values of the watermark
+    watermark_block_values = np.zeros(shape=(3, block_size))
+
+    # Assert that the available block size is larger than the watermark size
+    assert block_size > watermark_size, \
+        f"Not enough space available in the image ({block_size} bits) to extract the watermark ({watermark_size} bits)"
+
+    """
+    Initialize data structures for the extraction process. 
+    """
+    # Calculate the shape of the part of the image that contains the ca blocks and the indices of the blocks
+    ca_block_index = [(i, j) for i in range(ca_block_shape[0]) for j in range(ca_block_shape[1])]
+
+    # Randomized indices used to shuffle the positioning inside the blocks (same shuffle as in the embedding)
+    shuffle_indices = np.random.RandomState(rng_seed).random(size=(block_size, block_shape[0] * block_shape[1])) \
         .argsort(axis=1)
 
+    """
+    Extract the watermark bits from the image for each channel.
+    """
     for channel in range(3):
-        results = []
-        for i in range(block_num):
-            block = ca_block[channel][block_index[i]]
-            shuffler = idx_shuffle[i]
-            block_dct_shuffled = cv2.dct(block).flatten()[shuffler].reshape(block_shape)
+        # Iterate through every block in the image
+        for i in range(block_size):
+            # Get the current ca block and perform a Discrete Cosine Transform on it
+            block = ca_block[channel][ca_block_index[i]]
+            block_dct = cv2.dct(block)
 
+            # Get the shuffled indices for the current block and use them to shuffle the DCT coefficients block
+            shuffler = shuffle_indices[i]
+            block_dct_shuffled = block_dct.flatten()[shuffler].reshape(block_shape)
+
+            # Perform a Singular Value Decomposition on the shuffled DCT coefficients block
+            # Extract the watermark value from the first two singular values and add it to the stored values
             u, s, v = np.linalg.svd(block_dct_shuffled)
-            wm = (s[0] % d1 > d1 / 2) * 1
+            watermark_value = (s[0] % d1 > d1 / 2) * 1
             if d2:
                 tmp = (s[1] % d2 > d2 / 2) * 1
-                wm = (wm * 3 + tmp * 1) / 4
-            result = wm
-            results.append(result)
-        wm_block_bit[channel, :] = results
+                watermark_value = (watermark_value * 3 + tmp * 1) / 4
+            watermark_block_values[channel, i] = watermark_value
 
-    wm_avg = np.zeros(shape=wm_size)
-    for i in range(wm_size):
-        wm_avg[i] = wm_block_bit[:, i::wm_size].mean()
+    """
+    Reconstruct and return the watermark.
+    """
+    # Calculate the mean of the watermark values across all channels and repeated values
+    watermark_values_mean = np.zeros(shape=watermark_size)
+    for i in range(watermark_size):
+        watermark_values_mean[i] = watermark_block_values[:, i::watermark_size].mean()
 
-    wm_index = np.arange(wm_size)
-    np.random.RandomState(password_wm).shuffle(wm_index)
-    wm_avg[wm_index] = wm_avg.copy()
-    wm = wm_avg
+    # Reverse shuffle the watermark values (values were shuffled in the embedding)
+    wm_index = np.arange(watermark_size)
+    np.random.RandomState(rng_seed).shuffle(wm_index)
+    watermark_values_mean[wm_index] = watermark_values_mean.copy()
 
-    wm = 255 * wm.reshape(wm_shape[0], wm_shape[1])
+    # Transform the watermark values into a valid image and restore the shape
+    watermark = watermark_values_mean
+    watermark = 255 * watermark.reshape(watermark_shape[0], watermark_shape[1])
 
-    return wm
+    return watermark
